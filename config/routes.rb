4732f05ca734542fbfb3111ef3b1e8bdf8b82234
King601 <kaptoye@gmail.com>
2014-08-13 22:07:14 -0500

    Suggestion Form/Page/Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CookingWithKing::Application.routes.draw do
   root 'pages#home'
+  get 'suggestions' => 'pages#suggestions'
+  get 'suggest' => 'pages#suggestions'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
