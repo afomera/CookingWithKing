9b96bb6d43269ff1077d33e60dfb9e3c5b6aca3a
King601 <kaptoye@gmail.com>
2014-08-13 19:54:49 -0500

    Initial Commit of Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CookingWithKing::Application.config.secret_key_base = '9e653dd20e9231e4a1cc02704fbd109a0cd4c04ef2193cabdbfd68d137f9023c3ac615340b4313a8b4564edb9244bb6893019addbc13f462f20f50f9073802c8'
